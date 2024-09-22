8f3a5f7a74c12803a76bc3dde81971f4d9be46ac
Mikko <realmiks27@gmail.com>
2024-09-22 16:47:13 -0700

    add update and delete post routes

--- controllers/postController.js
+++ controllers/postController.js
@@ -17,8 +17,44 @@ exports.getPost = asyncHandler(async(req, res, next) => {
     res.json(post)
 })
 
+exports.updatePost = [
+    body("title").trim().notEmpty().withMessage("title cannot be empty"),
+    body("content").trim(),
+
+    asyncHandler(async(req, res, next) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            console.log(errors)
+            return res.status(400).json({ errors: errors.array()})
+        }
+        
+        const { title, content } = req.body
+        const updatedPost = await prisma.post.update({
+            where: {
+                id: parseInt(req.params.id)
+            },
+            data: {
+                title,
+                content
+            }
+        })
+
+        res.json(updatedPost)
+    })
+]
+
+exports.deletePost = asyncHandler(async(req, res, next) => {
+    await prisma.post.delete({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    })
+
+    res.json({ message: "Successfully deleted post" })
+})
+
 exports.createPost = [
-    body("title").trim().notEmpty().withMessage("email cannot be empty"),
+    body("title").trim().notEmpty().withMessage("title cannot be empty"),
     body("content").trim(),
 
     asyncHandler(async(req, res, next) => {
