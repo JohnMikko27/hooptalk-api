6012c5b99ddf427fc6bf62af541ff9f60366f6fd
Mikko <realmiks27@gmail.com>
2024-09-23 15:30:02 -0700

    remove email from db and auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,12 +5,11 @@ const { body, validationResult } = require("express-validator")
 const prisma = new PrismaClient()
 
 exports.createUser = [
-    body("email").trim().notEmpty().withMessage("email cannot be empty"),
-    body("username").trim().notEmpty().withMessage("username cannot be empty"),
-    body("password").trim().notEmpty().withMessage("password cannot be empty"),
+    body("username").trim().notEmpty().withMessage("Username cannot be empty."),
+    body("password").trim().notEmpty().withMessage("Password cannot be empty."),
     body('confirmPassword').custom((value, { req }) => {
         return value === req.body.password;
-    }),
+    }).withMessage("Passwords must match."),
 
     asyncHandler(async(req, res, next) => {
         const errors = validationResult(req)
@@ -19,14 +18,20 @@ exports.createUser = [
             return res.status(400).json({ errors: errors.array()})
         }
 
-        const { email, username, password } = req.body
+        const { username, password } = req.body
+        const existingUser = await prisma.user.findUnique({
+            where: { username }
+        })
+        if (existingUser) {
+            res.status(400).json({ message: "Username already exists. Choose a different one."})
+        }
         const user = await prisma.user.create({
             data: {
-                email,
                 username,
                 password
             }
         })
+
         res.json(user)
     })
 ]
