b87e7cd209f44f8d546de15abdbab25c3a53fdcc
Mikko <realmiks27@gmail.com>
2024-12-23 16:47:59 -0800

    add like/unlike comment functionality

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -52,3 +52,33 @@ exports.updateComment = [
     res.json({ status: 200, message: "Comment successfully updated.", comment: updatedComment });
   })
 ];
+
+exports.likeComment = asyncHandler(async(req, res, next) => {
+  const comment = await prisma.comment.findUnique({
+    where: { id: parseInt(req.params.commentId) }
+  })
+  const alreadyLiked = comment.usersLiked.includes(req.user.id)
+  if (alreadyLiked) {
+    const updatedComment = await prisma.comment.update({
+      where: { id: parseInt(req.params.commentId) },
+      data: {
+        upvotes: { decrement: 1 },
+        usersLiked: { set: comment.usersLiked.filter(id => id !== req.user.id )}
+      }, 
+      include: { post: true, author: true }
+    })
+
+    return res.json({ status: 200, message: "Comment successfully unliked.", comment: updatedComment })
+  } 
+  
+  const updatedComment = await prisma.comment.update({
+    where: { id: parseInt(req.params.commentId) },
+    data: {
+      upvotes: { increment: 1 },
+      usersLiked: { push: req.user.id }
+    },
+    include: { post: true, author: true }
+  })
+
+  res.json({ status: 200, message: "Comment successfully liked.", comment: updatedComment })
+})
